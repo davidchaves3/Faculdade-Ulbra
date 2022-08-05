6135d4735e23e3ad662501df72d4b4da8d4f29e8
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-08-05 16:15:30 -0300

    aula

---
+++ algoritmos/exercícios/ex003.py
@@ -0,0 +1,6 @@
+print('======== EXERCÍCIO 03 ========')
+num1 = float( input('Digite um número:'))
+num2 = float( input('Digite outro número:'))
+
+print(' A soma dos números é ', num1 + num2)
+print('O dobro da soma dos números é' , (num1+num2)*2)
---
+++ algoritmos/exercícios/ex004.py
@@ -0,0 +1,5 @@
+print('============ EXERCÍCIO 04 ============')
+
+peso = float(input('Digite o peso do prato cheio [Kg] :'))
+
+print('O valor que dever pagar é ', peso * 30.00)
