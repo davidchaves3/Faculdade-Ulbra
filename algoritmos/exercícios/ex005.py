d1cab1b4ad4246b5526955ff50ccfc02f7533911
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-08-05 20:48:12 -0300

    exercicio

---
+++ algoritmos/exercícios/ex005.py
@@ -0,0 +1,10 @@
+print('============= EXERCÍCIO 05 =============')
+
+nota1 = float( input('Valor da Prova 1° ==>'))
+nota2 = float(input('Valor da Prova 2° ==>'))
+nota3 = float(input('Valor da Prova 3° ==>'))
+
+media = ( nota1 + nota2 + nota3)/ 6
+
+print('A sua média é ', media)
+
