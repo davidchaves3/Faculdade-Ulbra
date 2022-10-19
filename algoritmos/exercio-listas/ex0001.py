1a72c41d53ae3f8219126d120069ccacb4b22200
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-10-19 09:53:21 -0300

    Exercício 1
    Exercício  1 aula/semana 11

---
+++ algoritmos/exercio-listas/ex0001.py
@@ -0,0 +1,28 @@
+from ast import While
+
+
+cont = 0 
+listaNum = []
+
+while cont < 6 :
+  num =listaNum.append(int(input("Digite um número inteiro:")))
+  cont = cont + 1 
+
+print("\n=================== TODOS OS NÚMEROS ===================")
+cont = 0
+
+while cont < len(listaNum) :
+  print(listaNum[cont])
+  cont = cont + 1 
+
+cont = 0 
+
+print("\n=================== 3 PRIMEIROS NÚMEROS ===================\n")
+
+while cont < 3 :
+  print(listaNum[cont])
+  print("====================================================\n")
+  cont = cont + 1
+
+
+
