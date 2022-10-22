bcf7dc1561274a2d4e1b170d103810265de977d4
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-10-21 23:18:25 -0300

    ex

---
+++ algoritmos/exercio-listas/ex0004.py
@@ -0,0 +1,27 @@
+listWord = []
+cont = 0
+
+print("===========================================")
+user = input("Digite uma palavra:")
+print("===========================================")
+
+while user != "sair":
+  listWord.append(user)
+  print("===========================================")
+  user = input("Digite outra palvra ou escreva SAIR para encerrar:")
+  print("===========================================")
+
+print("===========================================")
+print(listWord)
+print("===========================================")
+word = input("escolha uma palavra da lista:")
+print("===========================================")
+
+while cont < len(listWord):
+  if word == listWord[cont]:
+    listWord[cont] = "ELIMINADA"
+  cont = cont + 1
+
+print("===========================================")
+print(listWord)
+print("\n================ FINALIZADO ===============\n")
