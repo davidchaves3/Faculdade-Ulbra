0808d5e0478b292aee2b40fc054896f64989bae1
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-10-22 17:20:35 -0300

    update

--- algoritmos/exercio-listas/ex0005.py
+++ algoritmos/exercio-listas/ex0005.py
@@ -2,13 +2,18 @@ listNumber = [-5,10,-8,-3,5,10,11,8,-9,10]
 numberPositivo = []
 numberNegativo = [] 
 cont = 0
-while cont < len(listNumber):
-  if len(listNumber[cont]) > 0 :
-    numberPositivo.append(listNumber[cont])
-  elif len(listNumber[cont]) < 0 :
-    numberNegativo.append(listNumber[cont])
 
-print("Numeros Positivos:")
-print(numberPositivo)
-print("Numeros Negativos:")
-print(numberNegativo)
+while cont < len(listNumber):
+  num = listNumber[cont]
+  if num > 0:
+    numberPositivo.append(listNumber[cont])
+  elif num < 0:
+    numberNegativo.append(listNumber[cont])
+  cont = cont + 1
+
+print("================================================")
+print("Numeros Positivos:\n {}".format(numberPositivo))
+print("================================================")
+print("Numeros Negativos:\n {}".format(numberNegativo))
+print("================================================")
+
