3d3ef23bac4bf4f2bc422542b5d9153a5c2ec2fb
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-10-29 15:48:38 -0300

    ex

--- algoritmos/exercio-listas/ex0011.py
+++ algoritmos/exercio-listas/ex0011.py
@@ -1,17 +1,1 @@
 number = [5,7,3,2,8,10,9,9]
-auxiliar = 0
-auxiliar2 = 0
-cont = 0
-
-if len(number) %2 == 0:
-  while cont < len(number):
-    if number[cont] %2 == 0:
-     auxiliar = number[cont]
-     auxiliar2 = number[cont + 1]
-    
-     number[cont] = auxiliar2
-     number[cont + 1] = auxiliar
-      
-  print(number)
-else:
-  print("Lista não tem tamanho correto!!")
