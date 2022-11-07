2d3a9cab5edbf67864851631854c3818e5631860
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-11-07 13:30:41 -0300

    update

--- algoritmos/exercicios-lista-aninhadas/ex0002.py
+++ algoritmos/exercicios-lista-aninhadas/ex0002.py
@@ -2,17 +2,23 @@ participantes = []
 participante = []
 notaFinal = []
 cont = 0
+maior = 0
 
 while cont < 3 :
   participante.append(float(input("Nota do 1ª Jurado:")))
   participante.append(float(input("Nota do 2ª Jurado:")))
   participante.append(float(input("Nota do 3º Jurado:")))
-  notaFinal.append(participante[0] + participante[1] + participante[2])
+  notaFinal.append( participante[0] + participante[1] + participante[2])
   participantes.append(participante)
   participante = []
   cont = cont + 1
 
 cont = 0
+while cont < len(notaFinal):
+  if notaFinal[cont] > maior:
+    maior = notaFinal[cont]
+
+cont = 0
 while cont < len(participantes):
-  print(f"Código Participante {cont} NOTAS - {participante[cont][cont]}")
+  print(f"Código Participante {cont} NOTAS - {participantes[cont][0],participantes[cont][1],participantes[cont][2]}")
   cont = cont + 1
