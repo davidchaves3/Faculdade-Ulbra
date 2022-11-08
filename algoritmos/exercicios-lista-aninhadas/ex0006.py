00e3b00e7f6e2501e3d732e18b7b461913f1b90c
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-11-08 11:39:50 -0300

    update

--- algoritmos/exercicios-lista-aninhadas/ex0006.py
+++ algoritmos/exercicios-lista-aninhadas/ex0006.py
@@ -1,8 +1,26 @@
 signo = ['Macaco', 'Galo', 'Cão', 'Porco', 'Rato', 'Boi', 'Tigre', 'Coelho', 'Dragão', 'Serpente', 'Cavalo', 'Carneiro']
+signoNum = [0,1,2,3,4,5,6,7,8,9,10,11]
 usuarios = []
 cont = 0
+i = 0
 
-while cont < 10:
-  dadosIndivual = []
-  dadosIndivual.append(input("Nome:"))
-  dadosIndivual.append(int(input("Ano de Nascimento:")))
+while cont < 3:
+  dadosIndividual = []
+  dadosIndividual.append(input("Nome:"))
+  dadosIndividual.append(int(input("Ano de Nascimento:")))
+  calc = dadosIndividual[1]%2
+
+  while i < len(signoNum):
+    if calc == signoNum[i]:
+      dadosIndividual.append(signo[signoNum[i]])
+    i = i + 1  
+
+  usuarios.append(dadosIndividual)
+  cont = cont + 1
+
+print(usuarios)
+cont = 0
+while cont < len(usuarios):
+  print(usuarios[cont])
+  cont = cont + 1
+  
