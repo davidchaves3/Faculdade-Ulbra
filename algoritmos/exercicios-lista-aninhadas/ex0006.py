5519f06546c4c430a1bcde60853d4c4717de112e
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-11-08 08:26:01 -0300

    ex0006

---
+++ algoritmos/exercicios-lista-aninhadas/ex0006.py
@@ -0,0 +1,8 @@
+signo = ['Macaco', 'Galo', 'Cão', 'Porco', 'Rato', 'Boi', 'Tigre', 'Coelho', 'Dragão', 'Serpente', 'Cavalo', 'Carneiro']
+usuarios = []
+cont = 0
+
+while cont < 10:
+  dadosIndivual = []
+  dadosIndivual.append(input("Nome:"))
+  dadosIndivual.append(int(input("Ano de Nascimento:")))
